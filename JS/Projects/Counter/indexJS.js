55af7a26a88bb0f22c5eb315671fdf27c4de21c1
Kapil Sharma <capishr57@gmail.com>
2022-10-15 16:33:08 +0530

    added functionality of increment and save button

--- JS/Projects/Counter/indexJS.js
+++ JS/Projects/Counter/indexJS.js
@@ -1,6 +1,14 @@
-
+let peopleEl = document.getElementById('count-el');
 let people = 0;
 function increment() {
     people += 1;
-    document.getElementById('count-el').innerText = people;
+    peopleEl.textContent = people;
+}
+
+let saveEl = document.getElementById('save-el');
+function save() {
+    var saveCount = people + ' - ';
+    saveEl.textContent += saveCount;
+    people = 0;
+    peopleEl.textContent = people;
 }
